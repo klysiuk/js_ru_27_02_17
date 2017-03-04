c0677c8a84846f3e1eea0dc57fa1f7ba7291c8b4
Khrystyna Lysiuk <khrystyna.lysiuk@irco.com>
2017-03-04 18:26:51 +0200

    HomeTask1:  Add CommentList to Article

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,31 +1,45 @@
 import React, {Component} from 'react'
+import CommentList from './CommentList'
+
 
 class Article extends Component {
 
-    constructor() {
-        super()
-        this.state = {
-            isOpen: false
-        }
-    }
+	constructor() {
+		super()
+		this.state = {
+			isOpen: false,
+			commentsVisible: false
+		}
+	}
 
-    render() {
-        const {article} = this.props
-        const {isOpen} = this.state
-        const body = isOpen ? <section>{article.text}</section> : null
-        return (
-            <div>
-                <h3 onClick={this.handleClick}>{article.title}</h3>
-                {body}
-            </div>
-        )
-    }
+	render() {
+		const {article} = this.props
+		const {isOpen} = this.state
+		const body = isOpen ? <section>{article.text}</section> : null
+		const commentsButton = <button onClick={this.toggleComments}> {this.state.commentsVisible ? 'Hide' : 'Show'}</button>
+		const commentList = this.state.commentsVisible ? [<CommentList key='comments' comments={article.comments}/>,<p key='p'>{commentsButton}</p>] : null;
 
-    handleClick = (ev) => {
-        this.setState({
-            isOpen: !this.state.isOpen
-        })
-    }
+		return (
+			<div>
+			<h3 onClick={this.handleClick}>{article.title}</h3>
+			{body}
+			{commentsButton}
+			{commentList}
+			</div>
+		)
+	}
+
+	handleClick = (ev) => {
+		this.setState({
+			isOpen: !this.state.isOpen
+		})
+	}
+
+	toggleComments = (ev) => {
+		this.setState({
+			commentsVisible: !this.state.commentsVisible
+		});
+	}
 }
 
 export default Article
