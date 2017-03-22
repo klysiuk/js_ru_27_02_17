d5cb227d17b45756e9a756684c5bd212071a49fd
Roman <kaboom.ua@gmail.com>
2017-03-22 23:05:09 +0200

    Update articles.js

--- src/reducer/articles.js
+++ src/reducer/articles.js
@@ -9,6 +9,7 @@ export default (state = normalizedArticles, action) => {
             return state.filter(article => article.id !== payload.id)
 		case ADD_COMMENT:
 			let affectedArticle = state.find(article => article.id == payload.parentId);
+		        //здесь ты мутируешь стейт. Возвращаешь новый массив, но внутри меняешь объекты по ссылке
 			affectedArticle.comments.push(payload.comment.id);
 			return [...state, affectedArticle];
     }
