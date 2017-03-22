3ee51f5ff1d0e4093b57a633f55ad03dec032c4e
Roman <kaboom.ua@gmail.com>
2017-03-22 23:05:50 +0200

    Update generateId.js

--- src/middlewares/generateId.js
+++ src/middlewares/generateId.js
@@ -3,8 +3,10 @@ import { ADD_COMMENT } from '../constants'
 
 export default store => next => action => {
 	const { type } = action
+        //через мидлвары будет проходить каждый экшин, они должны быть максимально общими, завязывать на конкретные экшины - плохая практика
 	if (type == ADD_COMMENT) {
 		var updatedAction = {...action}
+	        //и лучше не мутировать payload, мало-ли что там станут передавать
 		updatedAction.payload.comment.id = uuidV4();
 		next(updatedAction)
 	} else {
